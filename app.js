9b2a55cdc5955c11160cd60a896df9045579d5d8
detoxfarm3 <dey.ronak@gmail.com>
2019-02-23 15:21:01 +0530

    logging app start

--- app.js
+++ app.js
@@ -18,7 +18,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.use((req, res, next) => {
+  console.log('Application starting...')
+  next();
+})
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
