6d3fc3d0702638cf6b8fd78a79809fa04bc93aee
detoxfarm3 <dey.ronak@gmail.com>
2019-02-28 08:52:20 +0530

    ooo

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {  
   console.log('test')
-  res.render('index', { title: 'Express1' });
+  res.render('index', { title: 'Express10' });
 });
 
 module.exports = router;
